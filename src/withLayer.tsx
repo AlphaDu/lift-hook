a05fb047755ad073c76230d763cab476c30130bb
Isaac <isaac.du811@outlook.com>
2024-02-01 15:04:50 +0800

    refactor: GIT 提交信息：
    重构代码并调整测试参数
    
    - 修改了 `src/withLayer.tsx` 中的导入声明
    - 重新定义 `withLayer` 函数，以利用 `JSX.IntrinsicAttributes`
    
    记住翻译所有给出的git提交消息。

--- src/withLayer.tsx
+++ src/withLayer.tsx
@@ -1,8 +1,7 @@
 import { Model } from "./liftHook"
-import { ComponentType,} from "react"
+import {ComponentType, JSX } from "react"
 import React from "react"
-
-const withLayer = <T extends Model<any, any>, P extends {},C extends ComponentType<P>>(Model: T) => (Cmp: C) => {
+const withLayer = <T extends Model<any, any>>(Model: T) => <P extends JSX.IntrinsicAttributes>(Cmp: ComponentType<P>) => {
 	const Ret = (props: P) => {
 		return (
 			<Model.Provider >
