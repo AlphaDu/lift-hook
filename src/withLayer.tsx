176180073eb61059b37463849396e866c5cced54
Isaac <isaac.du811@outlook.com>
2024-07-09 23:36:01 +0800

    fix: content

--- src/withLayer.tsx
+++ src/withLayer.tsx
@@ -2,14 +2,17 @@ import { Model } from "./liftHook"
 import { memo, FC } from "react"
 import React from "react"
 
-const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {},>(Cmp: FC<P>) => {
+const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {}, >(Cmp: FC<P>) => {
 	const Ret = (props: P) => {
+		const Reduced = Models.reduce((Acc, Before, index) => {
+			const { Provider } = Before
+			return (props: P) => <Provider>
+				<Acc {...props} />
+			</Provider>
+		}, Cmp)
 		return (
 			<>
-				{Models.reduceRight((acc, Model) => {
-					const { Provider } = Model
-					return <Provider>{acc}</Provider>
-				}, <Cmp {...props} />)}
+				<Reduced {...props} />
 			</>
 		)
 	}
