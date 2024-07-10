58b40e694a813b73627928758b94bad4ab7ee54b
Isaac <isaac.du811@outlook.com>
2024-07-10 17:16:58 +0800

    fix: layer order

--- src/withLayer.tsx
+++ src/withLayer.tsx
@@ -2,21 +2,26 @@ import { Model } from "./liftHook"
 import { memo, FC } from "react"
 import React from "react"
 
-const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {}, >(Cmp: FC<P>) => {
-	const Ret = (props: P) => {
-		const Reduced = Models.reduce((Acc, Before, index) => {
-			const { Provider } = Before
-			return (props: P) => <Provider>
-				<Acc {...props} />
-			</Provider>
-		}, Cmp)
-		return (
-			<>
-				<Reduced {...props} />
-			</>
-		)
-	}
-	return memo(Ret)
+
+const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {},>(Cmp: FC<P>) => {
+    const Ret = (props: P) => {
+        const Reduced = Models.reduceRight((Acc, Before, index) => {
+            const { Provider } = Before
+
+            const Ret = (props: P) => (<Provider>
+                <Acc {...props} />
+            </Provider>)
+            Ret.displayName = 'LayerWrapper_' + Provider.displayName;
+            return Ret
+        }, Cmp)
+        return (
+            <>
+                <Reduced {...props} />
+            </>
+        )
+    }
+    return memo(Ret)
 }
+
 
 export default withLayer
