cde68033d8458ef5deeb3f41c0a3aec78ef00c51
Isaac <isaac.du811@outlook.com>
2024-07-26 16:46:02 +0800

    feat: prod template annotation

--- src/withLayer.tsx
+++ src/withLayer.tsx
@@ -3,24 +3,26 @@ import { memo, FC } from "react"
 import React from "react"
 
 
-const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {},>(Cmp: FC<P>) => {
-    const Ret = (props: P) => {
-        const Reduced = Models.reduceRight((Acc, Before, index) => {
-            const { Provider } = Before
+const withLayer = <T extends Model<any, void>[]>(...Models: T) => <P extends {}, >(Cmp: FC<P>) => {
+	const Ret = (props: P) => {
+		const Reduced = Models.reduceRight((Acc, Before, index) => {
+			const { Provider } = Before
 
-            const Ret = (props: P) => (<Provider>
-                <Acc {...props} />
-            </Provider>)
-            Ret.displayName = 'LayerWrapper_' + Provider.displayName;
-            return Ret
-        }, Cmp)
-        return (
-            <>
-                <Reduced {...props} />
-            </>
-        )
-    }
-    return memo(Ret)
+			const Ret = (props: P) => (
+				<Provider  {...props}>
+					<Acc {...props} />
+				</Provider>
+			)
+			Ret.displayName = "LayerWrapper_" + Provider.displayName
+			return Ret
+		}, Cmp)
+		return (
+			<>
+				<Reduced {...props} />
+			</>
+		)
+	}
+	return memo(Ret)
 }
 
 
